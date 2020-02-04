91ee6f05f7c573c101c71c607035874c00e94a9c
fnzahhar <fnzahhar@gmail.com>
2020-02-04 10:10:46 +0000

    adding the keywords into our block

--- blocks/firstblock/index.js
+++ blocks/firstblock/index.js
@@ -8,6 +8,8 @@ registerBlockType('mytheme-blocks/firstblock',{
    category: 'layout',
    // adding icon from developer.wordpress.org/resource/dashicons
    icon: 'admin-network',
+   // keywords is a searchable terms, here we are making it translatable
+   keywords:[__('photo','mytheme-blocks'),__('image','mytheme-blocks')],
    edit: function(){
        return el('p',null,'Editor');
    },
